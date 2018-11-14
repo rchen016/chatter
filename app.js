83c3e728df9a6495603062f9677df9e38eda4c38
Chen <rchen@CompanyNet.org>
2018-11-14 10:18:33 -0800

    basic messages

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-var express = require("express");
+var express = require("express")
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
 var app = express();
 
-app.get("/",function(req,res){
-	res.send("Up");
+
+var MessageSchema = mongoose.model("Message",{
+	name: String,
+	message : String
 });
 
-var server = app.listen(3000,()=>{
-	console.log("Up");
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//var url = "mongodb://rcrc:zx1230321@ds161183.mlab.com:61183/chatapp";
+mongoose.connect("mongodb://localhost/chat_app");
+app.get("/",function(req,res){
+	res.render("index.ejs");
+});
+
+app.get("/message", function(req,res){
+	MessageSchema.find({},function(err,found){
+		res.send(found);
+	});
+});
+
+app.post("/message",function(req,res){
+	console.log("holla");
+	var name = "dsgsddfsfs";
+	var text = req.body.message;
+	MessageSchema.create({name:name,message:text},function(err){
+	 	if(err){
+			console.log("er");
+		}
+	});
+	res.redirect("back");
+	return;
+});
+
+app.listen(3000, process.env.IP, function(){
+  console.log("Server Up...");
 });
